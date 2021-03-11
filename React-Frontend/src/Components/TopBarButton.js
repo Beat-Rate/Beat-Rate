19c0b9cc63d4f7a51c4ddfd7dad48e8cf5ca0f97
RonaldColyar <PhantexCreations@gmail.com>
2021-03-11 05:23:04 -0600

    Converted PlayModal to dark theme

--- React-Frontend/src/Components/TopBarButton.js
+++ React-Frontend/src/Components/TopBarButton.js
@@ -1,12 +1,22 @@
 import React from 'react'
 
-
+function gatherOnClick(parent){
+    if (parent.props.id == "upload-topbar"){   
+        window.location.replace("/upload")
+    }
+}
 
 export default class TopBarButton extends React.Component {
+    constructor(props){
+        super(props);
+
+
+    }
     render() {
         return (
             <div className = "topbar-buttons"
             id = {this.props.id}
+            onClick = {()=>{gatherOnClick(this)}}
         
             >
                 <img  src = {this.props.image}></img>
