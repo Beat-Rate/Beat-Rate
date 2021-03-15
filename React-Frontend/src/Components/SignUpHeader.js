0e9bcfd6b54d68ba58b32b862321a58b584a13ff
RonaldColyar <PhantexCreations@gmail.com>
2021-03-15 01:42:17 -0500

    init sign up header

---
+++ React-Frontend/src/Components/SignUpHeader.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default class SignUpHeader extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Beat Rate</h1>
+                <p>
+                Get unfiltered , honest reviews in 
+                a few clicks. Release your latest music
+                with confidence.
+                </p>
+
+                <h3>
+                Get 30 free credits when you sign up.
+
+                </h3>
+
+                <p>
+                    By creating an account, you confirm that
+                    you've read and accept our <a>terms of use</a> 
+                    and <a>privacy policy</a>.
+
+                </p>
+                
+            </div>
+        )
+    }
+}
