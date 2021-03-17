4d51b9306de9bb051edebb44e769b6cf01363ea1
RonaldColyar <PhantexCreations@gmail.com>
2021-03-16 21:48:06 -0500

    removed un-used comps

--- React-Frontend/src/Components/MainCTA.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class MainCTA extends React.Component{
-    render(){
-        return(
-            <button className="main-cta" onClick={this.props.onClick}>{this.props.buttonTitle}</button>
-        );
-    }
-}
