19ded565f360663a4b109f9681e037561ef32ea9
RonaldColyar <phantexcreations@gmail.com>
2021-03-20 03:43:30 -0500

    basic architecture

--- React-Frontend/src/Components/ReviewReportPopup.js
+++ React-Frontend/src/Components/ReviewReportPopup.js
@@ -1,15 +1,26 @@
 import React from 'react'
-
+import trollicon from '../Components/Images/trollicon.png';
+import hateicon from '../Components/Images/hateicon.png';
+import inappropriateicon from '../Components/Images/inappropriateicon.png';
+import lowqualityicon from '../Components/Images/lowqualityicon.png';
+import ReviewReportButton from './ReviewReportButton';
+//css
+import "../Css/ReportReview.css";
 export default class ReviewReportPopup extends React.Component {
     render() {
-        //hate speech , low quality , troll,inappropriate
+
         return (
             <div id = "report-review-popup">
                 <button id = "close-popup">X</button>
                 <h1>Report Review!</h1>
                 <div id = "report-row-1">
-                    
+                    <ReviewReportButton img = {trollicon} label = "trolling"/>
+                    <ReviewReportButton img = {hateicon} label = "hate speech"/>
 
+                </div>
+                <div id = "report-row-2">
+                    <ReviewReportButton img = {inappropriateicon} label = "inappropriate"/>
+                    <ReviewReportButton img = {lowqualityicon}  label = "low quality"/>
                 </div>
             </div>
         )
