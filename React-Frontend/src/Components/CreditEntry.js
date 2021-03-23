6251f988c717ea51f2735a998a46f1f3289665bc
RonaldColyar <phantexcreations@gmail.com>
2021-03-23 00:21:40 -0500

    credit topbar init with styling

--- React-Frontend/src/Components/CreditEntry.js
+++ React-Frontend/src/Components/CreditEntry.js
@@ -6,29 +6,33 @@ import CreditEntryBottom from './CreditEntryBottom'
 export default class CreditEntry extends React.Component {
     render() {
         return (
-            <div>
-                <input id = "creditinput"></input>
-                <CreditEntryRow 
-                num1 = "1"
-                num2 = "2"
-                num3 = "3"            
+            <center>
+                <div id = "credit-parent">
+                    <input
+                         id = "creditinput"
+                         placeholder = "Credits..."></input>
+                    <CreditEntryRow 
+                    num1 = "1"
+                    num2 = "2"
+                    num3 = "3"            
+                        />
+                    <CreditEntryRow
+                    num1 = "4"
+                    num2 = "5"
+                    num3 = "6"
+                    
                     />
-                <CreditEntryRow
-                num1 = "4"
-                num2 = "5"
-                num3 = "6"
-                
-                />
-                <CreditEntryRow
-                num1 = "7"
-                num2 = "8"
-                num3 = "9"
-                />
-                <CreditEntryBottom/>
-                <button id = "complete">Continue</button>
-                
-                
-            </div>
+                    <CreditEntryRow
+                    num1 = "7"
+                    num2 = "8"
+                    num3 = "9"
+                    />
+                    <CreditEntryBottom/>
+                    <button id = "complete">Continue</button>
+                    
+                    
+                </div>
+            </center>
         )
     }
 }
