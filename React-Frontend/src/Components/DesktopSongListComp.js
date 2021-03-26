eb934d5557246f817e8efa4f0b0926b5c28a70d9
RonaldColyar <phantexcreations@gmail.com>
2021-03-26 01:16:03 -0500

    minor

--- React-Frontend/src/Components/DesktopSongListComp.js
+++ React-Frontend/src/Components/DesktopSongListComp.js
@@ -1,9 +1,13 @@
-import React from 'react'
-
+import React from 'react';
+import playbttn from '../Components/Images/play-img.png';
 export default class DesktopSongListComp extends React.Component {
     render() {
         return (
-            <div>
+            <div className = "desktop-list-comp">
+                <b></b>
+                <img
+                    src = {playbttn}
+                    className = "desktop-play-img"></img>
                 
 
 
