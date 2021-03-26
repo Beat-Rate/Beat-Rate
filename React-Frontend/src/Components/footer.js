ebe241fc91df8e9dd0d2febcdae744120eb04278
RonaldColyar <phantexcreations@gmail.com>
2021-03-26 01:29:01 -0500

    footer and contact page init

--- React-Frontend/src/Components/footer.js
+++ React-Frontend/src/Components/footer.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import logo from "../Components/Images/logowithnobg.png";
 export default class footer extends React.Component {
     render() {
         return (
             <div>
+                <ul>
+                    <li>Terms</li>
+                    <li>Privacy</li>
+                    <li>
+                        <img src = {logo}></img>
+                    </li>
+                    <li>Contact</li>
+                    <li>About
+                    </li>
+
+                </ul>
                 
             </div>
         )
