4739e4a04b79aec088e9c214240e5f7096a64f79
RonaldColyar <phantexcreations@gmail.com>
2021-03-30 02:26:59 -0500

    desktop styling and logic

--- React-Frontend/src/Screens/DesktopScreen.js
+++ React-Frontend/src/Screens/DesktopScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import "../Css/DesktopHome.css";
 import logo from "../Components/Images/logowithnobg.png";
-import DesktopBottomBar from '../Components/DesktopBottomBar';
+import DesktopBottomBar from '../Components/DesktopBottomBar.js';
+import HomeFunctionality from '../Components/HomeFunctionality.js';
+import DesktopSongListComp from '../Components/DesktopSongListComp.js';
 export default class DesktopHomeScreen extends React.Component {
 
 
@@ -10,6 +12,10 @@ export default class DesktopHomeScreen extends React.Component {
         this.state = {
             songList : []
         }
+        this.functionality = new HomeFunctionality(this);
+    }
+    componentDidMount(){
+        this.functionality.gatherData();
     }
     render() {
 
@@ -19,12 +25,16 @@ export default class DesktopHomeScreen extends React.Component {
             <div id = "desktop-parent">
                 <img id = "desktop-logo" src = {logo}></img>
                 <h2 id = "desktop-songs-header"> My Songs</h2>
-                <div id = "desktop-list-container">
-
-                </div>
+   
                 <DesktopBottomBar/>
-                <h1 id = "no-song-msg">Looks like you haven't uploaded any songs yet... </h1>
-                
+                <h1 
+                    style = {{display: (this.state.songList.length > 0?  "none":"block" )}}
+                    id = "no-song-msg">Looks like you haven't uploaded any songs yet... </h1>
+                <div id = "desktop-list-container">
+                    {this.state.songList.map(song =>{
+                        return <DesktopSongListComp song = {song}/>
+                    })}
+                </div>
             </div>
         )
     }
